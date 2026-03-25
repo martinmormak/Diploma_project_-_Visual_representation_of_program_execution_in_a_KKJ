e767e89b523ea8393f7f255465dd80fdbc36325d
Martin Mormak <martin.mormak@student.tuke.sk>
2026-03-25 21:47:30 +0100

    fix: add description for command help

--- frontend/src/components/HelpPopup.js
+++ frontend/src/components/HelpPopup.js
@@ -7,30 +7,30 @@ function HelpPopup() {
 
             <ul>
                 <li><b>ADD</b> – add top two numbers</li>
-                <li><b>SUB</b> – subtract second from top</li>
+                <li><b>SUB</b> – subtract top from second (second − top)</li>
                 <li><b>MUL</b> – multiply top two numbers</li>
                 <li><b>CMP</b> – compare top two numbers</li>
 
-                <li><b>NOT</b></li>
-                <li><b>AND</b></li>
-                <li><b>ISNEG</b></li>
-                <li><b>ISPOS</b></li>
+                <li><b>NOT</b> – logical negation of top value</li>
+                <li><b>AND</b> – logical AND of top two values</li>
+                <li><b>ISNEG</b> – check if top value is negative</li>
+                <li><b>ISPOS</b> – check if top value is positive</li>
 
-                <li><b>CLEAR</b></li>
-                <li><b>OVER</b></li>
-                <li><b>POP</b></li>
-                <li><b>DUP</b></li>
-                <li><b>SWAP</b></li>
-                <li><b>ROTL</b></li>
-                <li><b>ID</b></li>
+                <li><b>CLEAR</b> – clear the stack</li>
+                <li><b>OVER</b> – copy second item to top</li>
+                <li><b>POP</b> – remove top item</li>
+                <li><b>DUP</b> – duplicate top item</li>
+                <li><b>SWAP</b> – swap top two items</li>
+                <li><b>ROTL</b> – rotate stack elements left</li>
+                <li><b>ID</b> – identity (no operation)</li>
 
-                <li><b>{`{ ... }`}</b></li>
-                <li><b>APPLY</b></li>
-                <li><b>APPLYOVER</b></li>
-                <li><b>COMPOSE</b></li>
+                <li><b>{`{ ... }`}</b> – quotation (code block)</li>
+                <li><b>APPLY</b> – execute quotation</li>
+                <li><b>APPLYOVER</b> – execute quotation preserving original value</li>
+                <li><b>COMPOSE</b> – combine two quotations</li>
 
-                <li><b>CHOOSE</b></li>
-                <li><b>WHILE</b></li>
+                <li><b>CHOOSE</b> – conditional execution (if-else)</li>
+                <li><b>WHILE</b> – loop while condition holds</li>
             </ul>
 
             <hr />
